2cd70f9feff3ab3ce45de86888d42677fffc2791
Steve Ndemanou <steve.ndemanou@advens.fr>
2024-07-27 10:22:07 +0200

    Add missing alt prop on image tag

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
             />
         </div>
       </div>
